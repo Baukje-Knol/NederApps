11ec66a916be613c08e21c22894511421f3b89c6
Baukje-Knol <knolbl@gmail.com>
2018-08-30 14:33:27 +0200

    again

--- index.js
+++ index.js
@@ -4,4 +4,6 @@ const app = express();
 // app.get('/',(req,res) => res.send('Hello'));
 app.use(express.static('./src'))
 
-app.listen(3000);
+app.listen(3000,function(){
+console.log("Server is running on port: 3000");
+})
